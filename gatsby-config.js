94b64b0a3abb5d1c61190a58fbc161290e80df58
Ciaran <ciaran.edwards@live.com>
2018-09-14 07:59:13 +0100

    Add font-awesome and other icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'White Market Podcast',
   },
   plugins: [
     'gatsby-plugin-resolve-src',
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,21 @@
 import React, { Fragment } from 'react';
 import { node } from 'prop-types';
 import Helmet from 'react-helmet';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import 'styles/all.sass';
 import 'styles';
-
 import Navbar from './Navbar';
+
+library.add(fas, far, fab);
+// Kicks off the process of finding <i> tags and replacing with <svg>
+dom.watch();
 
 const TemplateWrapper = ({ children }) => (
   <Fragment>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="White Market Podcast | Free Music and Free Culture" />
     <Navbar />
     <main>{children}</main>
   </Fragment>
