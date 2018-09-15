b31232eaeb27f68255bd4fb666d17308af584dab
Ciaran <ciaran.edwards@live.com>
2018-09-15 19:44:47 +0100

    Update the font again ^_^

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
         google: {
-          families: ['Zilla Slab', 'Quicksand'],
+          families: ['Crete Round', 'Quicksand'],
         },
       },
     },
