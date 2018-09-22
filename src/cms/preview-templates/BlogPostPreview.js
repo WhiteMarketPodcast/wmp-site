38add04d22a832071213c3de509b87051feda32b
Ciaran <ciaran.edwards@live.com>
2018-09-22 15:04:40 +0100

    Upgrade netlify cms. Testing BlogPostPreview error

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,21 +1,29 @@
+import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BlogPostTemplate } from '../../templates/blog-post';
+import { BlogPostTemplate } from 'templates/blog-post';
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
-  <BlogPostTemplate
-    content={widgetFor('body')}
-    description={entry.getIn(['data', 'description'])}
-    tags={entry.getIn(['data', 'tags'])}
-    title={entry.getIn(['data', 'title'])}
-  />
-);
+const BlogPostPreview = ({ entry, widgetFor }) => {
+  const tags = entry.getIn(['data', 'tags']);
+  console.log('tags', tags);
+  const tailTags = _.get(tags, '_tail.array');
+  console.log('tailTags', tailTags);
+
+  return (
+    <BlogPostTemplate
+      content={widgetFor('body')}
+      description={entry.getIn(['data', 'description'])}
+      tags={tailTags || tags}
+      title={entry.getIn(['data', 'title'])}
+    />
+  );
+};
 
 BlogPostPreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
-  }),
-  widgetFor: PropTypes.func,
+  }).isRequired,
+  widgetFor: PropTypes.func.isRequired,
 };
 
 export default BlogPostPreview;
