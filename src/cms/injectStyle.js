3a9f83bb784ee91e79088310a77a2d0b43efd8ce
Ciaran <ciaran.edwards@live.com>
2018-11-06 07:29:19 +0000

    Revert change

--- src/cms/injectStyle.js
+++ src/cms/injectStyle.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import GlobalStyle from 'styled-components';
+import { StyleSheetManager } from 'styled-components';
 
-const injectStyle = (WrappedComponent) => (props) => (
-    <>
-      <GlobalStyle />
+const injectStyle = (WrappedComponent) => (props) => {
+  const iframe = document.querySelector('iframe');
+  const target = iframe.contentDocument.head;
+
+  return (
+    <StyleSheetManager target={target}>
       <WrappedComponent {...props} />
-    </>
-);
+    </StyleSheetManager>
+  );
+};
 
 export default injectStyle;
