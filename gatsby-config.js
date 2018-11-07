fbf4f86963f0efff01f74e09e959b39fe4e1cd10
Ciaran <ciaran.edwards@live.com>
2018-11-07 06:42:10 +0000

    Revert changes to netlify cms plugin. Seems to work now :/

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,13 +62,7 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: `gatsby-plugin-netlify-cms`,
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-        enableIdentityWidget: true,
-      },
-    },
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-netlify`, // make sure to keep it last in the array
   ],
 };
